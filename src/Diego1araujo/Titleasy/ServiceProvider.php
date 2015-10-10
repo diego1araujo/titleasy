b8891b82725236a0347db5654608d535e576ee38
Diego Araujo <diegoishigh@gmail.com>
2015-10-10 19:52:06 -0300

    replace indent to 4 spaces and not using tab

--- src/Diego1araujo/Titleasy/ServiceProvider.php
+++ src/Diego1araujo/Titleasy/ServiceProvider.php
@@ -4,44 +4,42 @@ namespace Diego1araujo\Titleasy;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var boolean
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var boolean
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+    }
 
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['titleasy'] = $this->app->share(function($app) {
+            return new Titleasy;
+        });
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['titleasy'] = $this->app->share(function($app) {
-			return new Titleasy;
-		});
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('titleasy');
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('titleasy');
+    }
 }
--- src/Diego1araujo/Titleasy/Titleasy.php
+++ src/Diego1araujo/Titleasy/Titleasy.php
@@ -4,63 +4,60 @@ namespace Diego1araujo\Titleasy;
 
 /**
  * Titleasy
- *
  * Build titles like a charm
- *
- * Package by Diego Araujo <diego77araujo@gmail.com>
  * Based on Yunseok Kim's HTML Title Generator
+ *
+ * @author Diego Araujo <diego77araujo@gmail.com>
  */
 class Titleasy
 {
+    /**
+     * The titles
+     *
+     * @var array
+     */
+    protected static $titles = array();
 
-	/**
-	 * The titles
-	 *
-	 * @var array
-	 */
-	protected static $titles = array();
-
-	/**
-	 * Add title
-	 *
-	 * @return void
-	 */
-	public static function put()
-	{
+    /**
+     * Add title
+     *
+     * @return void
+     */
+    public static function put()
+    {
 		$args = func_get_args();
 
 		foreach($args as $arg) {
-			if ( ! empty($arg)) {
-				static::$titles[] = trim(strip_tags($arg));
-			}
-		}
-	}
+            if ( ! empty($arg)) {
+                static::$titles[] = trim(strip_tags($arg));
+            }
+        }
+    }
 
-	/**
-	 * Returns last added title
-	 *
-	 * @return string
-	 */
-	public static function last()
-	{
-		return end(static::$titles);
-	}
+    /**
+     * Returns last added title
+     *
+     * @return string
+     */
+    public static function last()
+    {
+        return end(static::$titles);
+    }
 
-	/**
-	 * Returns generated title for display
-	 *
-	 * @param  string  $default_title
-	 * @param  string  $delimiter
-	 * @param  bool    $reverse
-	 * @return string
-	 */
-	public static function get($default_title = NULL, $delimiter = '-', $reverse = FALSE)
-	{
-		if ( ! is_null($default_title)) {
-			array_unshift(static::$titles, $default_title);
-		}
+    /**
+     * Returns generated title for display
+     *
+     * @param  string  $default_title
+     * @param  string  $delimiter
+     * @param  boolean $reverse
+     * @return string
+     */
+    public static function get($default_title = "", $delimiter = "-", $reverse = false)
+    {
+        if ( ! is_null($default_title)) {
+            array_unshift(static::$titles, $default_title);
+        }
 
-		return implode(' ' . $delimiter . ' ', $reverse === TRUE ? array_reverse(static::$titles) : static::$titles);
-	}
-
+        return implode(' '.$delimiter.' ', $reverse === true ? array_reverse(static::$titles) : static::$titles);
+    }
 }
